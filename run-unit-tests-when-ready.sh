503579df57752f6d80e92977770e988854364940
Ned Henry <ethenry@ucsc.edu>
2020-04-17 11:47:38 -0700

    fix directory of waiting script

--- run-unit-tests-when-ready.sh
+++ run-unit-tests-when-ready.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-/srv/hycruz/wait-for-services.sh localhost:3000 -t
-/srv/hycruz/wait-for-services.sh db:3306 -t 180
-/srv/hycruz/wait-for-services.sh repo:8080 -t 180
-/srv/hycruz/wait-for-services.sh index:8983 -t 180
+/srv/wait-for-services.sh localhost:3000 -t
+/srv/wait-for-services.sh db:3306 -t 180
+/srv/wait-for-services.sh repo:8080 -t 180
+/srv/wait-for-services.sh index:8983 -t 180
 bundle exec rspec /srv/hycruz/spec/unit
