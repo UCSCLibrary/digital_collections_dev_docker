d0458c68b471b1ca83d8ef4731a25bf7bc7b687d
Ned Henry <ethenry@ucsc.edu>
2020-04-17 11:50:03 -0700

    wait a few minutes for the web application to start up before timing out

--- run-unit-tests-when-ready.sh
+++ run-unit-tests-when-ready.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-/srv/wait-for-services.sh localhost:3000 -t
+/srv/wait-for-services.sh localhost:3000 -t 300
 /srv/wait-for-services.sh db:3306 -t 180
 /srv/wait-for-services.sh repo:8080 -t 180
 /srv/wait-for-services.sh index:8983 -t 180
