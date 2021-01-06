b56f389c183261b9acd68e867387dc914a6ece83
Ned Henry <ethenry@ucsc.edu>
2021-01-06 15:09:41 -0800

    Try to get Coveralls to run by adding CI environment variable explicitly to rspec command

--- run-unit-tests-when-ready.sh
+++ run-unit-tests-when-ready.sh
@@ -3,4 +3,4 @@
 /srv/wait-for-services.sh db:3306 -t 180
 /srv/wait-for-services.sh repo:8080 -t 180
 /srv/wait-for-services.sh index:8983 -t 180
-bundle exec rspec /srv/hycruz/spec/unit
+CI=true bundle exec rspec /srv/hycruz/spec/unit
