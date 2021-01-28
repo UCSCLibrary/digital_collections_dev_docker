3f2cca8c85ec834c4e1f9eba8d35067673bb5362
Ned Henry <ethenry@ucsc.edu>
2021-01-27 16:42:41 -0800

    Run more tests

--- run-tests-when-ready.sh
+++ run-tests-when-ready.sh
@@ -3,4 +3,4 @@
 /srv/wait-for-services.sh db:3306 -t 180
 /srv/wait-for-services.sh repo:8080 -t 180
 /srv/wait-for-services.sh index:8983 -t 180
-CI=true COVERALLS_REPO_TOKEN=$COVERALLS_REPO_TOKEN bundle exec rspec /srv/hycruz/spec/unit /srv/hycruz/spec/acceptance
+CI=true COVERALLS_REPO_TOKEN=$COVERALLS_REPO_TOKEN bundle exec rspec spec --exclude-pattern "spec/integration/bulk_ops/*/*_spec.rb, spec/smoke/*_spec.rb"
